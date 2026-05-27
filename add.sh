317b9236af68d7ce88f5619f70bf087026ff7708
Rohit <rohits78009@gmail.com>
2026-05-27 09:08:33 +0000

    add file added

---
+++ add.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input two numbers
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# Add numbers
+sum=$((num1 + num2))
+
+# Display result
+echo "Sum is: $sum"
