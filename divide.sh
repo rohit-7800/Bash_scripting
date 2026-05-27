fb87e47b8c4f0da2d4cb5f3f8ea293e4e93cc6d5
Rohit <rohits78009@gmail.com>
2026-05-27 09:39:43 +0000

    divide file added

---
+++ divide.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input two numbers
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# divide numbers
+div=$((num1 / num2))
+
+# Display result
+echo "div is: $div"
