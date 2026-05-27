3a82d50163ffedca1db9046f033be10aa366078d
Rohit <rohits78009@gmail.com>
2026-05-27 09:20:43 +0000

    subtract  file  added

---
+++ subtract.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input two numbers
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# subtract two numbers
+sub=$((num1 - num2))
+
+# Display result
+echo "Sub is: $sub"
