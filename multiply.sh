0e126d77390ef14248a76590a798d35b61b2e404
Rohit <rohits78009@gmail.com>
2026-05-27 09:30:46 +0000

    multiply file added

---
+++ multiply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input two numbers
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# multiply numbers
+mul=$((num1 * num2))
+
+# Display result
+echo "mul is: $mul"
